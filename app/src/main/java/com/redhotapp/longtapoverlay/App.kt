a4b098ece3aa6fc26904c7905df028abdb8d767f
Antonio <akogan777@gmail.com>
2019-12-16 14:04:46 +0200

    starting service.

---
+++ app/src/main/java/com/redhotapp/longtapoverlay/App.kt
@@ -0,0 +1,22 @@
+package com.redhotapp.longtapoverlay
+
+//
+// Created by Antonio on 2019-12-16.
+// email: akogan777@gmail.com
+//
+
+
+import android.app.Application
+
+class App : Application() {
+//
+//
+//    override fun onCreate() {
+//        super.onCreate()
+//
+////        startService(Intent(this, LongTapService::class.java))
+//
+//    }
+
+
+}
--- app/src/main/java/com/redhotapp/longtapoverlay/MainActivity.kt
+++ app/src/main/java/com/redhotapp/longtapoverlay/MainActivity.kt
@@ -1,15 +1,55 @@
 package com.redhotapp.longtapoverlay
 
+import android.app.Activity
 import android.content.Intent
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.Settings
 import androidx.appcompat.app.AppCompatActivity
+import org.jetbrains.anko.longToast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_main)
+        cmdTurnCursorServiceOn()
+    }
 
-        startService(Intent(this, LongTapService::class.java))
+
+    private fun cmdTurnCursorServiceOn() {
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (!Settings.canDrawOverlays(this)) {
+                try {
+                    val intent = Intent(
+                        Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                        Uri.parse("package:" + this.getPackageName())
+                    )
+                    startActivityForResult(intent, 1234)
+                } catch (e: Exception) {
+                }
+            } else {
+                startService(Intent(this, LongTapService::class.java))
+
+            }
+        } else {
+            startService(Intent(this, LongTapService::class.java))
+        }
+
+    }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        // Check which request we're responding to
+        if(intent != null)
+        if (requestCode == 1234) {
+            // Make sure the request was successful
+            if (resultCode == Activity.RESULT_OK) {
+                startService(Intent(this, LongTapService::class.java))
+
+            }else{
+                longToast(R.string.overlay_was_denied)
+            }
+        }
     }
 }
